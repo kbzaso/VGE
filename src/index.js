db2ed58f62cec1637b557a6f63539d1afb53eaa5
kbzaso <alejandro.sapa@gmail.com>
2021-08-26 19:52:47 -0400

    Seccion preferirnos mobile slide [OK]

--- src/index.js
+++ src/index.js
@@ -25,6 +25,7 @@ import '@styles/main.scss';
     footer.innerHTML = Footer();
 })();
 
+// MENU MOBILE
 const btnHamburger = document.getElementById('btnHamburger');
 const header = document.querySelector('.header');
 const overlay = document.querySelector('.overlay');
@@ -41,6 +42,78 @@ btnHamburger.addEventListener('click', function() {
     }
 });
 
-// Slider preferirnos
-const perfIzq = document.getElementById('preferIzq');
-const prefDer = document.getElementById('preferDer');
+// PREFERIRNOS SLIDER
+const track = document.querySelector('.preferirnos__cards');
+const slides = Array.from(track.children);
+
+// BOTONES DE DESPLAZAMIENTO
+const nextButton = document.querySelector('.preferirnos__botonera--right');
+const prevButton = document.querySelector('.preferirnos__botonera--left');
+
+// // CIRCULOS DE DESPLAZAMIENTO
+const dotNav = document.querySelector('.preferirnos__nav');
+const dots = Array.from(dotNav.children);
+
+// // CAPTURAMOS EL ANCHO DE LOS SLIDES
+const slideWidth = slides[0].getBoundingClientRect().width;
+
+// // COLOCAR UN SLIDE AL LADO DEL OTRO
+const setSlidePosition = (slides, index) => {
+    slides.style.left = slideWidth * index + 'px';
+};
+
+slides.forEach(setSlidePosition);
+
+const moveToSlide = (track, currentSlide, targetSlide) => {
+    track.style.transform = 'translateX(-' + targetSlide.style.left + ')';
+    currentSlide.classList.remove('current-card');
+    targetSlide.classList.add('current-card');
+};
+
+// UPDATE DOTS
+const updateDots = (currentDot, targetDot) => {
+    currentDot.classList.remove('current-indicator');
+    targetDot.classList.add('current-indicator');
+};
+
+// console.log(slides);
+
+// // CUANDO HAYA UN CLICK SE MUEVA AL SLIDE DE LA DERECHA
+nextButton.addEventListener('click', (e) => {
+    const currentSlide = track.querySelector('.current-card');
+    const nextSlide = currentSlide.nextElementSibling;
+    const currentDot = dotNav.querySelector('.current-indicator');
+    const nextDot = currentDot.nextElementSibling;
+
+    // MOVER HACIA EL SIGUIENTE SLIDE
+    moveToSlide(track, currentSlide, nextSlide);
+    updateDots(currentDot, nextDot);
+});
+
+prevButton.addEventListener('click', (e) => {
+    const currentSlide = track.querySelector('.current-card');
+    const prevSlide = currentSlide.previousElementSibling;
+    const currentDot = dotNav.querySelector('.current-indicator');
+    const prevDot = currentDot.previousElementSibling;
+
+    // MOVER HACIA EL SIGUIENTE SLIDE
+    moveToSlide(track, currentSlide, prevSlide);
+    updateDots(currentDot, prevDot);
+});
+
+// CUANDO HAGO CLICL EN LOS INDICADORES, SE MOVERA AL SLIDE
+dotNav.addEventListener('click', (e) => {
+    // QUE INDICADOR FUE CLICKEADO?
+    const targetDot = e.target.closest('button');
+    if (!targetDot) return;
+
+    const currentSlide = track.querySelector('.current-card');
+    const currentDot = dotNav.querySelector('.current-indicator');
+
+    const targetIndex = dots.findIndex((dot) => dot === targetDot);
+    const targetSlide = slides[targetIndex];
+
+    moveToSlide(track, currentSlide, targetSlide);
+    updateDots(currentDot, targetDot);
+    // console.log(targetIndex);
+});
--- src/templates/preferirnos.js
+++ src/templates/preferirnos.js
@@ -3,44 +3,53 @@
 import helmet from '@images/preferirnos/helmet.svg';
 import eficiencia from '@images/preferirnos/eficiencia.svg';
 import respaldo from '@images/preferirnos/respaldo.svg';
+
 const Preferirnos = () => {
     const view = `
-    <div class="preferirnos hide-for-mobile">
-        <div class="preferirnos__content container">
-            <h2>Por qué Preferirnos</h2>
-            <h3>Uniendo tres ramas de la Ingeniería Civil, buscamos proyectar nuestra nueva propuesta como una opción segura y confiable.</h3>
             
-            <div class="preferirnos__botonera hide-for-desktop">
-                <a href=""><</a>
-                <a href="">></a>
-            </div>
-            
-            <div class="preferirnos__cards">
-                <div class="preferirnos__tab">
-                    <img src="${helmet}" alt="Icono de respaldo" />
-                    <h2>Expertiz</h2>
-                    <p>
-                    En VGE Ingeniería, convergen tres campos de la Ingeniería civil en búsqueda de innovar en la industria y proponer un nuevo estándar en el desarrollo de soluciones estructurales.
-                    </p>
-                </div>
-                <div class="preferirnos__tab">
-                    <img src="${eficiencia}" alt="Icono de eficiencia" />
-                    <h2>Eficiencia</h2>
-                    <p>
-                    Estamos dedicados a la optimización de nuestros procesos, buscando ser un referente en cuanto a pulcritud de nuestro trabajo y la práctica inversión del tiempo. 
-                    </p>
-                </div>
-                <div class="preferirnos__tab">
-                    <img src="${respaldo}" alt="Icono de confianza" />
-                    <h2>Respaldo</h2>
-                    <p>
-                    Nos motiva ser un sello de calidad, y demostrar nuestro compromiso, es parte de nuestras mayores fortalezas. 
-                    </p>
-                </div>
-            </div>
+    <div class="preferirnos">
+    
+        <div class="preferirnos__botonera hide-for-desktop">
+            <button type='button' class='preferirnos__botonera--left'><i class="fas fa-chevron-left"></i></button>
+            <button type='button' class='preferirnos__botonera--right'><i class="fas fa-chevron-right"></i></button>
+        </div>
 
+    <div class="preferirnos__content container container--px">
+        <h2>Por qué Preferirnos</h2>
+        <h3>Uniendo tres ramas de la Ingeniería Civil, buscamos proyectar nuestra nueva propuesta como una opción segura y confiable.</h3>
+        <div class="preferirnos__cards">
+            <div class="preferirnos__tab current-card expertiz">
+                <img src="${helmet}" alt="Icono de respaldo" />
+                <h2>Expertiz</h2>
+                <p>
+                En VGE Ingeniería, convergen tres campos de la Ingeniería civil en búsqueda de innovar en la industria y proponer un nuevo estándar en el desarrollo de soluciones estructurales.
+                </p>
+            </div>
+            <div class="preferirnos__tab eficiencia">
+                <img src="${eficiencia}" alt="Icono de eficiencia" />
+                <h2>Eficiencia</h2>
+                <p>
+                Estamos dedicados a la optimización de nuestros procesos, buscando ser un referente en cuanto a pulcritud de nuestro trabajo y la práctica inversión del tiempo. 
+                </p>
+            </div>
+            <div class="preferirnos__tab respaldo">
+                <img src="${respaldo}" alt="Icono de confianza" />
+                <h2>Respaldo</h2>
+                <p>
+                Nos motiva ser un sello de calidad, y demostrar nuestro compromiso, es parte de nuestras mayores fortalezas. 
+                </p>
+            </div>
+        </div>
+            <div class="preferirnos__nav hide-for-desktop">
+                <button type='button' class="preferirnos__indicator current-indicator"></button>
+                <button type='button' class="preferirnos__indicator"></button>
+                <button type='button' class="preferirnos__indicator"></button>
+            </div>
         </div>
     </div>
+
+
+    
   `;
     return view;
 };
