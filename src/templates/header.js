20096caaee227381e9f019e849fc202cd1a644c4
kbzaso <alejandro.sapa@gmail.com>
2021-10-29 17:10:16 -0300

    Agregue npm package para favicon

--- src/templates/header.js
+++ src/templates/header.js
@@ -8,9 +8,7 @@ const Header = () => {
             <p>
                 Queremos entregarte soluciones prácticas e innovadoras, preocupados por la eficiencia desde su etapa conceptual hasta el desarrollo de la Ingeniería en detalle.
             </p>
-            <div class="icon">
-          <i class="fas fa-chevron-down"></i>
-          </div>
+
         </div>
     </section>
   `;
